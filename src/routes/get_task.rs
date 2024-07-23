cba86691e50f5554e18a50c21370266ad545fd25
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-07-23 14:19:49 +0530

    filter

---
+++ src/routes/get_task.rs
@@ -0,0 +1,61 @@
+use axum::{extract::{Path, Query}, http::StatusCode, Extension, Json};
+use sea_orm::{ ColumnTrait, Condition, DatabaseConnection, EntityTrait, QueryFilter};
+use serde::{Deserialize, Serialize};
+use crate::database::tasks::{self, Entity as Tasks};
+#[derive(Serialize, Deserialize)]
+pub struct ResponseTask{
+    id:i32,
+    title:String,
+    priority:Option<String>,
+    description:Option<String>
+}
+#[derive( Deserialize)]
+pub struct PriorityQuery{
+    priority:Option<String>
+}
+
+pub async fn get_one_task(
+    Path(task_id):Path<i32>,
+     Extension(database):Extension<DatabaseConnection>
+)-> Result<Json<ResponseTask>, StatusCode>{
+    let task = Tasks::find_by_id(task_id).one(&database).await.unwrap();
+    if let Some(task) = task{
+         Ok(Json(ResponseTask{
+            id:task.id,
+            title:task.title,
+            priority:task.priority,
+            description:task.description
+        }))
+    }else{
+        Err(StatusCode::NOT_FOUND)
+    }
+}
+
+pub async fn get_all_task(
+    Extension(database):Extension<DatabaseConnection>,
+    Query(q):Query<PriorityQuery>
+)->Result<Json<Vec<ResponseTask>>, StatusCode>{
+    let mut priority_filter =  Condition::all();
+    // creating a new filter
+    if let Some(priority) = q.priority{ // if only query exists
+        priority_filter = if priority.is_empty(){ //handling the condition where string=""
+            priority_filter.add(tasks::Column::Priority.is_null())
+        }else{
+            priority_filter.add(tasks::Column::Priority.eq(priority))
+        }
+    }
+    let tasks = Tasks::find()
+        .filter(priority_filter)
+        .all(&database)
+        .await
+        .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?
+        .into_iter()
+        .map(|task| ResponseTask{
+            id:task.id,
+            title:task.title,
+            priority:task.priority,
+            description:task.description
+        })
+        .collect();
+    Ok(Json(tasks)) 
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,16 +1,17 @@
 mod create_tasks;
-mod get_one_task;
+mod get_task;
 use create_tasks::create_tasks;
-use get_one_task::get_one_task;
-use sea_orm::{Database, DatabaseConnection};
+use get_task::{get_one_task, get_all_task};
+use sea_orm::DatabaseConnection;
 use axum::{
-    http::Method, middleware, routing::{get, post}, Extension, Router
+    http::Method, routing::{get, post}, Extension, Router
 };
 use tower_http::cors::{Any, CorsLayer};
 pub fn create_routes(database:DatabaseConnection)-> Router{
     let cors  = CorsLayer::new().allow_methods([Method::GET, Method::POST]).allow_origin(Any);
     let app = Router::new()
         .route("/tasks", post(create_tasks))
+        .route("/get_all_tasks", get(get_all_task))
         .route("/get_one_task/:id", get(get_one_task))
         .layer(Extension(database))
         .layer(cors);
