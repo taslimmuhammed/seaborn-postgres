14a5b32025120e9bd7cf24c9e2cbf2c519b2e542
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-07-23 11:40:51 +0530

    get one

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,8 @@
-use sea_orm::{Database, DatabaseConnection};
 mod create_tasks;
+mod get_one_task;
 use create_tasks::create_tasks;
+use get_one_task::get_one_task;
+use sea_orm::{Database, DatabaseConnection};
 use axum::{
     http::Method, middleware, routing::{get, post}, Extension, Router
 };
@@ -9,6 +11,7 @@ pub fn create_routes(database:DatabaseConnection)-> Router{
     let cors  = CorsLayer::new().allow_methods([Method::GET, Method::POST]).allow_origin(Any);
     let app = Router::new()
         .route("/tasks", post(create_tasks))
+        .route("/get_one_task/:id", get(get_one_task))
         .layer(Extension(database))
         .layer(cors);
     app
