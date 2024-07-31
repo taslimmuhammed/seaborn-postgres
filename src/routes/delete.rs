0ed4eaa3700d949744e1fada10b0cb4e843cf1c5
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-07-31 12:55:53 +0530

    delete

---
+++ src/routes/delete.rs
@@ -0,0 +1,47 @@
+use axum::{extract::Path, http::StatusCode, Extension};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter, Set};
+
+use crate::database::tasks;
+
+pub async fn delete_task(
+    Path(task_id):Path<i32>,
+    Extension(database):Extension<DatabaseConnection>
+)->Result<(),StatusCode>{
+    // let task = if let Some(task) = tasks::Entity::find_by_id(task_id)
+    //     .one(&database)
+    //     .await
+    //     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?{
+    //         task.into_active_model()
+    //     }else{ 
+    //         return Err(StatusCode::NOT_FOUND)
+    //     };
+    // tasks::Entity::delete(task)
+    //     .exec(&database)
+    //     .await
+    //     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    // Ok(())
+    tasks::Entity::delete_many()
+        .filter(tasks::Column::Id.eq(task_id))
+        .exec(&database)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(())
+}
+
+pub async fn soft_delete(
+    Path(task_id):Path<i32>,
+    Extension(database):Extension<DatabaseConnection>
+)-> Result<(), StatusCode>{
+    let mut task = if let Some(task) = tasks::Entity::find_by_id(task_id)
+        .one(&database)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?{
+            task.into_active_model()
+        }else{
+            return Err(StatusCode::NOT_FOUND);
+        };
+    let now = chrono::Utc::now();
+    task.deleted_at = Set(Some(now.into()));
+    
+    Ok(())
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,13 +2,15 @@ mod create_tasks;
 mod get_task;
 mod atomic_update;
 mod partial_update;
+mod delete;
+use delete::delete_task;
 use create_tasks::create_tasks;
 use get_task::{get_one_task, get_all_task};
 use atomic_update::atomic_update;
 use partial_update::partial_update;
 use sea_orm::DatabaseConnection;
 use axum::{
-    http::Method, routing::{get, patch, post, put}, Extension, Router
+    http::Method, routing::{delete, get, patch, post, put}, Extension, Router
 };
 use tower_http::cors::{Any, CorsLayer};
 pub fn create_routes(database:DatabaseConnection)-> Router{
@@ -19,6 +21,7 @@ pub fn create_routes(database:DatabaseConnection)-> Router{
         .route("/get_one_task/:id", get(get_one_task))
         .route("/atomic_update/:task_id", put(atomic_update))
         .route("/partial_update/:task_id",patch(partial_update))
+        .route("/delete/:task_id", delete(delete_task))
         .layer(Extension(database))
         .layer(cors);
     app
